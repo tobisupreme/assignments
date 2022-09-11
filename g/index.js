e9cb1ed1e580a0709ebf19299ffb31f6437f817d
Tobi Balogun <tobi.balo@gmail.com>
2022-09-11 03:11:38 +0100

    Add ability to create new user

--- g/index.js
+++ g/index.js
@@ -1,9 +1,75 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const userStorePath = path.join(__dirname, 'db', 'users.json')
+let users = []
 
 function createUser(req, res) {
-  res.writeHead(201)
-  const response = { message: 'You successfully sent a POST request to the users/create route' }
-  res.end(JSON.stringify(response))
+  console.log('creating user...')
+  const body = []
+
+  // get data from request
+  req.on('data', (data) => {
+    body.push(data)
+  })
+
+  req.on('end', () => {
+    const parsedData = Buffer.concat(body).toString()
+    const jsonData = JSON.parse(parsedData)
+
+    // read data from users.json
+    fs.readFile(userStorePath, 'utf-8', (err, data) => {
+      if (err) {
+        res.writeHead(500)
+        res.end(JSON.stringify({ error: 'Internal Server Error. Could not save user' }))
+        return
+      }
+
+      // and store it in
+      const userStore = JSON.parse(data)
+
+      // check if username to add already exists in users.json
+      const existsInStore = (store, newValue) => {
+        const check = store.find((user) => user.username.toLowerCase() === newValue.username.toLowerCase())
+        if (check) {
+          return true
+        }
+        return false
+      }
+
+      const check = existsInStore(userStore, jsonData)
+
+      // create a new user if not exists
+      if (!check) {
+        const newUser = {
+          firstName: jsonData.firstName || '',
+          lastName: jsonData.lastName || '',
+          username: jsonData.username,
+          email: jsonData.email,
+          role: 'standard',
+        }
+        userStore.push(newUser)
+
+        // save new users.json
+        fs.writeFile(userStorePath, JSON.stringify(userStore), (err) => {
+          if (err) {
+            res.writeHead(500)
+            res.end(JSON.stringify({ error: 'Internal Server Error. Could not save user' }))
+            return
+          }
+
+          res.writeHead(201)
+          res.end(JSON.stringify({ message: 'User successfully added', user: newUser }))
+        })
+        return
+      } else {
+        res.writeHead(400)
+        res.end(JSON.stringify({ error: `The username ${jsonData.username} is already taken` }))
+        return
+      }
+    })
+  })
 }
 
 function getAllUsers(req, res) {
